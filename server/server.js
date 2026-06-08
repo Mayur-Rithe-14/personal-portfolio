021527b1f593bed39d01534d57138977670149ba
Mayur Rithe <mayurrithe2004@gmail.com>
2026-06-09 00:30:15 +0530

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -14,13 +14,10 @@ const allowedOrigins = ["http://localhost:5173", process.env.CORS_ORIGIN];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://personal-portfolio-1-cqic.onrender.com",
+    ],
     credentials: true,
   }),
 );
