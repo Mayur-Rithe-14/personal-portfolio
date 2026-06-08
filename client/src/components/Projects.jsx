550fcdc5bf211b86e375bad7029421b84a7bc12c
Mayur Rithe <mayurrithe2004@gmail.com>
2026-06-08 21:30:05 +0530

    Initial Commit

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -14,7 +14,9 @@ export default function Projects() {
 
   const fetchProjects = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/projects");
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/projects`,
+      );
       const data = await response.json();
 
       if (data.success) {
